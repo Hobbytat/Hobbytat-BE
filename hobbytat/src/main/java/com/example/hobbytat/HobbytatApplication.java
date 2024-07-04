e610b07402f97d3ad1c3f9217df71191940d7d7c
Choi wontak <76509639+RoundTable02@users.noreply.github.com>
2024-07-04 19:22:03 +0900

    feat: git init

---
+++ hobbytat/src/main/java/com/example/hobbytat/HobbytatApplication.java
@@ -0,0 +1,13 @@
+package com.example.hobbytat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbytatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbytatApplication.class, args);
+	}
+
+}
---
+++ hobbytat/src/test/java/com/example/hobbytat/HobbytatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hobbytat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbytatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
