f2ab57c7b0491ae541233405538383abfef22c27
Choi wontak <76509639+RoundTable02@users.noreply.github.com>
2024-07-04 20:23:50 +0900

    feat: 기본 Repository

---
+++ src/main/java/com/example/hobbytat/repository/ArticleLikeRepository.java
@@ -0,0 +1,7 @@
+package com.example.hobbytat.repository;
+
+import com.example.hobbytat.domain.ArticleLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleLikeRepository extends JpaRepository<ArticleLike, Long> {
+}
