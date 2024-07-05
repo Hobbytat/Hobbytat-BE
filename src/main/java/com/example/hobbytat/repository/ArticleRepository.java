ab4caf7dd19c7735218a6377ed33c326473c33e6
Choi wontak <76509639+RoundTable02@users.noreply.github.com>
2024-07-06 02:12:50 +0900

    feat: Ranking 로직 추가

--- src/main/java/com/example/hobbytat/repository/ArticleRepository.java
+++ src/main/java/com/example/hobbytat/repository/ArticleRepository.java
@@ -1,8 +1,10 @@
 package com.example.hobbytat.repository;
 
 import com.example.hobbytat.domain.Article;
-import com.example.hobbytat.domain.ArticleLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    List<Article> findTop5ByViewCount();
 }
---
+++ src/main/java/com/example/hobbytat/service/RankingService.java
@@ -0,0 +1,58 @@
+package com.example.hobbytat.service;
+
+import com.example.hobbytat.controller.dto.response.SimpleArticleRankResponseDto;
+import com.example.hobbytat.controller.dto.response.SimpleTypeRankResponseDto;
+import com.example.hobbytat.domain.Article;
+import com.example.hobbytat.domain.HobbyType;
+import com.example.hobbytat.repository.ArticleRepository;
+import com.example.hobbytat.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class RankingService {
+    private final MemberRepository memberRepository;
+    private final ArticleRepository articleRepository;
+
+    public List<SimpleTypeRankResponseDto> getTypeRankings() {
+        long allCount = memberRepository.count();
+
+        HobbyType[] types = HobbyType.values();
+
+        List<SimpleTypeRankResponseDto> typeRankResponseDtoList = new ArrayList<>();
+        for (HobbyType type : types) {
+            long typeCount = memberRepository.countByHobbyType(type);
+            double percent = ((double) typeCount/allCount) * 100;
+            SimpleTypeRankResponseDto typeRankResponseDto = SimpleTypeRankResponseDto.builder()
+                    .hobbyType(type)
+                    .percent((int) percent)
+                    .build();
+            typeRankResponseDtoList.add(typeRankResponseDto);
+        }
+        typeRankResponseDtoList.sort(Comparator.comparingInt(SimpleTypeRankResponseDto::getPercent));
+
+        for (int i = 1; i < typeRankResponseDtoList.size() + 1; i++) {
+            typeRankResponseDtoList.get(i-1).setRank(i);
+        }
+
+        return typeRankResponseDtoList;
+    }
+
+    public List<SimpleArticleRankResponseDto> getArticleRankings() {
+        List<Article> articles = articleRepository.findTop5ByViewCount();
+
+        return articles.stream()
+                .map(SimpleArticleRankResponseDto::toDto)
+                .collect(Collectors.toList());
+    }
+}
